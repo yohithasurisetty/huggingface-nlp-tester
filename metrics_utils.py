e43b5b4f32c5b3f9987f9c0f91bf2c9588a97d87
yohithasurisetty <108508813+yohithasurisetty@users.noreply.github.com>
2025-07-19 13:33:03 +0530

    Update metrics_utils.py

--- metrics_utils.py
+++ metrics_utils.py
@@ -4,10 +4,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def evaluate_model(true, pred):
+    # Convert both lists to strings to prevent type mismatch
+
+true = [str(x).lower().strip() for x in true]
+pred = [str(x).lower().strip() for x in pred]
+
     acc = accuracy_score(true, pred)
     report = classification_report(true, pred)
     cm = confusion_matrix(true, pred)
     return acc, report, cm
+
 
 def plot_confusion(cm):
     fig, ax = plt.subplots()
